93255194a6cea153bce97402a152ddbcc2917a62
David-Inkheart <okoliedave1@gmail.com>
2023-02-11 15:59:23 +0100

    header and button

---
+++ src/components/Tasks.js
@@ -0,0 +1,30 @@
+const tasks = [
+    {
+        id: 1,
+        text: "Eleanor's Paediatrician Appointment",
+        day: 'Feb 13th at 1:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Valentine Dinner with Sunflower',
+        day: 'Feb 14th at 6:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: "Book club watch party",
+        day: 'Feb 17th at 8:30pm',
+        reminder: false,
+    },   
+]
+
+const Tasks = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Tasks
